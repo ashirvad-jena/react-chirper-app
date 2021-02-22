1f8e44c8f0cd89f38188a66aab2e9be128d781a7
Ashirvad Jena <>
2021-02-22 18:29:22 +0530

    added middlewares

---
+++ src/middleware/logger.js
@@ -0,0 +1,8 @@
+export default logger = (store) => (next) => (action) => {
+	console.group(action.type);
+	console.log("The action:", action);
+	const returnValue = next(action);
+	console.log("The new state is:", store.getState());
+	console.groupEnd();
+	return returnValue;
+};

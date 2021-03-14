800e24297e48f26c6bfd12b4a82a39cf3a878e7e
Ashirvad Jena <ashirvad.aj@yahoo.com>
2021-03-15 00:02:19 +0530

    added loader on top

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -2,13 +2,16 @@ import { getInitialData } from "../utils/api";
 import { receiveUsers } from "./users";
 import { receiveTweets } from "./tweets";
 import { setAuthedUser } from "./authedUser";
+import { showLoading, hideLoading } from "react-redux-loading";
 
 // Example of usage of thunk, action creator returns function instead of object
 const AUTHED_ID = "tylermcginnis";
 export const handleInitialData = () => (dispatch) => {
+	dispatch(showLoading());
 	return getInitialData().then(({ users, tweets }) => {
 		dispatch(receiveUsers(users));
 		dispatch(receiveTweets(tweets));
 		dispatch(setAuthedUser(AUTHED_ID));
+		dispatch(hideLoading());
 	});
 };
